f38dfaa8297821db9e459ce59ab31d8e966bd06e
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-11-10 01:37:23 +0900

    #2.7 Themes
    ## 개요
    
    - 이번에 배울 ****Themes**** 와 다음에 배울 local Estate Management 를 사용하면 다크모드를 구현 할 수 있다.
    
    ## ****Themes 이란?****
    
    - 모든 색상을 가지고 있는 Object
        - 색상 모음 세트 같은 것
    - 하나의 Object안에 모든 색상을 가지고 있기 때문에
    
        → 색상 모드를 바꿀 때 일일히 색상 값을 설정 하지 않고 한번에 **색상 버전**을 바꿀 수 있어서 편리하다.
    
    ## 사용법
    
    ### 1. `src/index.js` 파일
    
    ```jsx
    **import { ThemeProvider } from 'styled-components';**
    
    const darkTheme = {
      textColor: "whitesmoke",
      backgroundColor: "#111",
    };
    
    const lightTheme = {
      textColor: "#111",
      backgroundColor: "whitesmoke",
    };
    
    ReactDOM.render(
      <React.StrictMode>
        **<ThemeProvider theme={darkTheme}>
    		//<ThemeProvider theme={lightTheme}>**
          <App />
        **</ThemeProvider>**
      </React.StrictMode>,
      document.getElementById("root")
    );
    ```
    
    1. `src/index.js` 파일에 'styled-components'의 `ThemeProvider` 불러오기
    
        `import { ThemeProvider } from 'styled-components';`
    
    2. `ThemeProvide` 로 전체 컴포넌트 감싸주기
    
        → `ThemeProvide` 로 감싸진 내부에 있는 모든 컴포넌트에서 `theme`에 설정된 색상에 접근 할 수 있다.
    
    3. 각각의 `theme` object 제작
        - `theme` object 에서 사용하는 속성 이름은 색상명이 아니라, `textColor` 와 같이 요소의 이름으로 설정 한다.
            - 다른 버전의 색상 세트로 수정 할 경우를 대비
            - 다른 버전의 `theme` 에도 같은 속성명을 사용 하여야 한다.
    4. `ThemeProvide` 에 `theme` props 전달 해주기
        - 다른 버전의 색상 버전을 사용하고 싶으면, 다른 `theme` 을 props로 전달해 주기만 하면 된다.
    
    ### 2. `theme` 사용하기
    
    - props 로 전달되기 때문에 컴포넌트에서 props로 접근 할 수 있다.
        - 정확히는 `props.theme` 으로 접근 한다.
    
    ```jsx
    const Title = styled.h1`
    	color: **${(props) => props.theme.textColor};**
    	background-color: **${(props) => props.theme.backgroundColor};**
    `;
    ```

--- src/App.js
+++ src/App.js
@@ -6,31 +6,17 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as='div'>🤩</Emoji>
-      </Box>
-      <Emoji as='p'>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }

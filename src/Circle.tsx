ebac9010566dff1a02bfe8249df86715caa56aed
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-11-11 12:59:02 +0900

    #3.3 Optional Props
    ## 목표
    
    - **Default Props** 와 **Optional Props** 를 알아보자
    
    ## 배경
    
    - 그냥 interface로 정의하면, 그 값은 require(필수)값이 된다.
    - 만약, Props의 Type은 정의 하지만 필수값은 아니게 하고 싶다면 **Optional Props** 값으로 만들어야 한다.
    
    ## ****Optional Props****
    
    - 있어도 되고 없어도 되는 값
    
    ### 사용법
    
    ```jsx
    interface InterfaceName {
    	defaultProps: Type;
    	o**ptionalProps?:** Type**;**
    }
    ```
    
    - Property 이름 뒤에 물음표(`?`) 를 붙여주면, 해당 값은 **optional Props** 가 된다.
    
    ```jsx
    interface InterfaceName {
    	o**ptionalProps:** Type | undefined**;**
    }
    ```
    
    - 혹은 이렇게 작성해도 같은 뜻이지만, 물음표를 사용하는것이 더 짧은 방식이기에 편하다.
    
    ## ****Default Props****
    
    - 만약, 값이 주어지지 않는 경우 기본적으로 설정되는 값
    
    ### 사용법
    
    ```jsx
    ...
    return <Container props={props **?? 'value'**} />;
    ```
    
    - 전달되는 props 뒤에 물음표 두개(`??`)를 붙이고 그 뒤에 기본값을 작성한다.
    
    ```jsx
    function Component({props **= 'default value'**}){
    	...
    }
    ```
    
    - `TypeScript` 문법은 아니지만, JavaScript `ES6`의 기본 문법으로도 ****Default Props**** 를 설정 할 수 있다.
    - 컴포넌트의 받아오는 Props에 이퀄 기호(`=`)를 붙이고 그 뒤에 기본값을 작성

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-interface CircleProps {
-    bgColor: string
+interface ContainerProps {
+    bgColor: string;
+    borderColor: string;
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     border-radius: 100%;
-    background-color: ${props => props.bgColor}
+    background-color: ${props => props.bgColor};
+    border: 1px solie ${props => props.borderColor};
 `;
 
-function Circle({bgColor}: CircleProps) {
+interface CircleProps {
+    bgColor: string;
+    borderColor?: string;
+    text?: string;
+}
+
+function Circle({bgColor, borderColor, text = 'default text'}: CircleProps) {
     return (
-        <Container bgColor={bgColor} />
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} >
+            {text}
+        </Container>
     );
 }
 

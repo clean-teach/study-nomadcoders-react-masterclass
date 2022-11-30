2eb82df50cb66538b53dc7842df820c8cee10921
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-11-30 20:50:33 +0900

    #5.0 Setup
    ## 개요
    
    - 이제까지 배운 내용들(리액트, 타입스크립트, 스타일링, 데이터 패치 등..)을 복습해본다.
    
    ## 목표
    
    - 간단한 코인 트래킹 어플리케이션을 만들어 본다.
    
        ### 코인파프리카
    
        [Coinpaprika](https://api.coinpaprika.com/#tag/Tags)
    
        - 코인에 관한 정보(가격, 과거 가격 등)를 볼 수 있는 API
    - 이제까지 배운 내용을 활용해본다
    - 데이터를 Fetching 하는 섹시한 방법(React-query)을 배워본다.
    
        [React Query](https://react-query-v3.tanstack.com/)
    
        - 훨씬 편리한 방식으로 Data를 fetch 할 수 있는 패키지
        - React-query 를 배우기 전에 기본적인 방법으로 Data fetch 를 구현해보며 React-query와 비교해본다.
    
            > *“왜 이걸 사용하는지 이해하기 위해서는 먼저, **문제**를 이해 하여야 한다.”*
            >
    
    ### 프로젝트에 필요한 설치 패키지
    
    - `typescript`
    - `style-component`
    - `react-router-dom` : 본 강의는 5버전으로 녹화되었음
        - `npm i react-router-dom@5.3.0`
    - `react-query`
    - `styled-reset`
    
    ### 프로젝트 화면 구조
    
    - Home
        - 모든 코인을 보여주는 페이지
    - 상세 페이지
        - 파라미터 URL을 이용해서 `:id` 별로 각각의 코인 페이지에 접근
    - Nested(중첩된) Router
        - 한 스크린 내에 또 다른 Router를 가질 수 있는 것
    
    ## 구현
    
    ### 패키지 설치
    
    - 만약 `TypeScript`에서 인식하지 못하는 패키지가 있다면, `[@types/` 버전](https://www.notion.so/03-TYPESCRIPT-b66fa2079c8e48b7a4fdee1f2a2dbe18)이 있는지 확인해서 설치 한다.
    
        `$ npm i --save-dev @types/react-router-dom`
    
    ### 폴더 구조
    
    - `src`
        - `index.tsx`
        - `App.tsx`
        - `Router.tsx`
        - `routes`
            - `Coins.tsx`
            - `Coin.tsx`
    
    ### Router (5버전 기준)
    
    - `<BrowserRouter>` : Router를 사용하기 위하여 전체를 감싸는 컴포넌트
    - `<Switch>` : 한번에 하나의 Route만 보여줄 경우
    - `path` : 경로를 표기
    - root 경로의 경우 Route를 마지막에 작성해주어야 이상 없이 동작
    - `useParams()` : 현재 파라미터에 접근 할 수 있는 메서드
        - 타입스크립트의 경우 `interface`를 활용해서 타입을 지정 해줘야 할 수도 있다.
    
            ```tsx
            **interface RouteParams {
                coinId: string;
            }**
    
            function Coin() {
                const {coinId} = useParams**<RouteParams>**();
                ...
            }
            ```

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
-  const Container = styled.div`
-    background-color: ${props => props.theme.bgColor};
-  `;
-  const H1 = styled.h1`
-    color: ${props => props.theme.textColor};
-  `;
-  const Btn = styled.button`
-    background-color: ${props => props.theme.btnColor};
-  `;
-  const [value, setValue] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
-    const {
-      currentTarget: {value}
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Submit", value);
-  };
 
   return (
-    <Container>
-      <H1>Protected</H1>
-      <form onSubmit={onSubmit}>
-        <input type='text' placeholder='user name' value={value} onChange={onChange} />
-        <Btn>Log in</Btn>
-      </form>
-    </Container>
+    <Router />
   );
 }
 

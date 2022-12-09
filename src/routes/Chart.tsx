5e97faa074c8b6fa1aad9b935d5d051703cc5bc0
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-10 01:03:13 +0900

    #5.13 Price Chart part Two
    ### `Apexcharts`
    
    [ApexCharts.js - Open Source JavaScript Charts for your website](https://apexcharts.com/)
    
    - 자바스크립트 차트 라이브러리
    - **`Angular`, `React`, `Vue`** 등 다양한 자바스크립트 작업환경에서 사용 가능
    - React 사용법
    
        [React-ApexChart - A React Chart wrapper for ApexCharts.js](https://apexcharts.com/docs/react-charts/)
    
        1. `npm install --save react-apexcharts apexcharts` 설치
        2. **`import** ChartComponentName **from** "react-apexcharts";` 불러오기
        3. 불러온 차트 컴포넌트 렌더링
    
            ```tsx
            <ChartComponentName
            	options={{
                chart: {
                  id: "basic-bar"
                },
                xaxis: {
                  categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
                }
              }}
            	series={[
            	  {
            	    name: "series-1",
            	    data: [30, 40, 45, 50, 49, 60, 70, 91]
            	  }
            	]}
              type="bar"
              width="500"
            />
            ```
    
            - `Apexcharts` 문서를 참고해서 Props를 설정해준다.
                - [type](https://apexcharts.com/docs/options/chart/type/) : 차트의 종류를 고른다.
                - [series](https://apexcharts.com/docs/options/series/) :
                    - 차트에 전달 할 데이터
                    - Object로 이루어진 배열 형태이다.
                        - name
                        - data
                - [options](https://apexcharts.com/docs/options/annotations/#)  : 차트 스타일 등 각종 옵션을 선택 할 수 있다.

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,73 @@
 import { useQuery } from 'react-query';
 import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
 
 interface IChartProps {
   coinId: string;
 }
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 function Chart({ coinId }: IChartProps) {
-  const { isLoading, data } = useQuery(['ohlcv', coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(['ohlcv', coinId], () =>
     fetchCoinHistory(coinId),
   );
-  console.log(data);
-  return <h1>Chart</h1>;
+  // console.log(data?.map((price) => Number(price.close)));
+  return (
+    <div>
+      {isLoading ? (
+        'Loading Chart...'
+      ) : (
+        <ApexChart
+          type="area"
+          series={[
+            {
+              name: 'Close Price',
+              data: data?.map((price) => Number(price.close)) as number[],
+            },
+            {
+              name: 'Open Price',
+              data: data?.map((price) => Number(price.open)) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: 'transparent',
+            },
+            grid: { show: false },
+            stroke: {
+              curve: 'smooth',
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
